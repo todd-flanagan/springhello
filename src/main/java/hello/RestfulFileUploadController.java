e37db01db6a2188f413af8835f0321b65a84041a
Todd Flanagan <tflanagan@gmail.com>
2018-06-14 12:29:55 -0400

    restul uploader

--- src/main/java/hello/RestfulFileUploadController.java
+++ src/main/java/hello/RestfulFileUploadController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,13 +23,15 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 import java.util.Arrays;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.storage.StorageFileNotFoundException;
 import hello.storage.StorageService;
 
 
-@Controller
-public class FileUploadController {
+@RestController
+public class RestfulFileUploadController {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
@@ -39,35 +41,14 @@ public class FileUploadController {
     JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public FileUploadController(StorageService storageService) {
+    public RestfulFileUploadController(StorageService storageService) {
         this.storageService = storageService;
     }
 
-    @GetMapping("/")
-    public String listUploadedFiles(Model model) throws IOException {
 
-        model.addAttribute("files", storageService.loadAll().map(
-                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                        "serveFile", path.getFileName().toString()).build().toString())
-                .collect(Collectors.toList()));
-
-        return "uploadForm";
-    }
-
-    @GetMapping("/files/{filename:.+}")
-    @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-
-        Resource file = storageService.loadAsResource(filename);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    }
-
-    @PostMapping("/")
+    @RequestMapping("/upload")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
             RedirectAttributes redirectAttributes) {
-
-        log.info("uploading: " + file.getOriginalFilename());
 
         storageService.store(file);
         redirectAttributes.addFlashAttribute("message",
@@ -83,7 +64,7 @@ public class FileUploadController {
         jdbcTemplate.batchUpdate("INSERT INTO builds (toolbox, ctf) VALUES (?,?)", names);
 
 
-        return "redirect:/";
+        return "uploaded: " + file.getOriginalFilename();
     }
 
     @ExceptionHandler(StorageFileNotFoundException.class)
